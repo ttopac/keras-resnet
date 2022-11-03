7a4cc7ec6937412839cdad4f321d8c6c5179e302
tackoo <tanaytopac@stanford.edu>
2022-11-03 15:43:12 +0300

    changed ResNet2D18 to new style

--- keras_resnet/models/_2d.py
+++ keras_resnet/models/_2d.py
@@ -55,57 +55,75 @@ class ResNet2D(keras.Model):
     """
     def __init__(
         self,
-        inputs,
         blocks,
         block,
         include_top=True,
         classes=1000,
         freeze_bn=True,
         numerical_names=None,
+        name = "ResNet1D",
         *args,
         **kwargs
     ):
+        super(ResNet1D, self).__init__(name=name, **kwargs)
+        self.blocks = blocks
+        self.block = block
+        self.include_top = include_top
+        self.classes = classes
+        self.freeze_bn = freeze_bn
+        self.numerical_names = numerical_names
+        
         if keras.backend.image_data_format() == "channels_last":
-            axis = 3
+            self.axis = 3
         else:
-            axis = 1
+            self.axis = 1
 
         if numerical_names is None:
-            numerical_names = [True] * len(blocks)
+            self.numerical_names = [True] * len(blocks)
 
-        x = keras.layers.Conv2D(64, (7, 7), strides=(2, 2), use_bias=False, name="conv1", padding="same")(inputs)
-        x = keras_resnet.layers.ResNetBatchNormalization(axis=axis, epsilon=1e-5, freeze=freeze_bn, name="bn_conv1")(x)
-        x = keras.layers.Activation("relu", name="conv1_relu")(x)
-        x = keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding="same", name="pool1")(x)
+        self.conv2d = keras.layers.Conv2D(64, (7, 7), strides=(2, 2), use_bias=False, name="conv1", padding="same")
+        self.batchnormalization = keras_resnet.layers.ResNetBatchNormalization(axis=self.axis, epsilon=1e-5, freeze=self.freeze_bn, name="bn_conv1")
+        self.activation = keras.layers.Activation("relu", name="conv1_relu")
+        self.maxpooling2d = keras.layers.MaxPooling2D((3, 3), strides=(2, 2), padding="same", name="pool1")
+        self.globalaveragepooling2d = keras.layers.GlobalAveragePooling2D(name="pool5")
+        self.dense = keras.layers.Dense(self.classes, activation="softmax", name="fc1000")
+
 
         features = 64
-
-        outputs = []
-
         for stage_id, iterations in enumerate(blocks):
             for block_id in range(iterations):
-                x = block(
-                    features,
-                    stage_id,
-                    block_id,
-                    numerical_name=(block_id > 0 and numerical_names[stage_id]),
-                    freeze_bn=freeze_bn
-                )(x)
-
+                curr_block = block(features,
+                                    stage_id,
+                                    block_id,
+                                    numerical_name=(block_id > 0 and self.numerical_names[stage_id]),
+                                    freeze_bn=self.freeze_bn
+                                    )
+                self.blocklist.append(curr_block)
+            self.stagelist.append(self.blocklist)
+            self.blocklist = []
             features *= 2
 
+
+    def call (self, inputs, training=False):
+        x = self.conv2d(x)
+        x = self.batchnormalization(x)
+        x = self.activation(x)
+        x = self.maxpooling2d(x)
+
+        outputs = list()
+
+        for stage in self.stagelist:
+            for block in stage:
+                x = block(x)
             outputs.append(x)
 
-        if include_top:
-            assert classes > 0
-
-            x = keras.layers.GlobalAveragePooling2D(name="pool5")(x)
-            x = keras.layers.Dense(classes, activation="softmax", name="fc1000")(x)
-
-            super(ResNet2D, self).__init__(inputs=inputs, outputs=x, *args, **kwargs)
+        if self.include_top:
+            assert self.classes > 0
+            x = self.globalaveragepooling2d(x)
+            x = self.dense(x)
+            return x
         else:
-            # Else output each stages features
-            super(ResNet2D, self).__init__(inputs=inputs, outputs=outputs, *args, **kwargs)
+            return outputs
 
 
 class ResNet2D18(ResNet2D):
@@ -136,20 +154,21 @@ class ResNet2D18(ResNet2D):
 
         >>> model.compile("adam", "categorical_crossentropy", ["accuracy"])
     """
-    def __init__(self, inputs, blocks=None, include_top=True, classes=1000, freeze_bn=False, *args, **kwargs):
+    def __init__(self, blocks=None, include_top=True, classes=1000, freeze_bn=False, **kwargs):
         if blocks is None:
             blocks = [2, 2, 2, 2]
 
         super(ResNet2D18, self).__init__(
-            inputs,
             blocks,
             block=keras_resnet.blocks.basic_2d,
             include_top=include_top,
             classes=classes,
             freeze_bn=freeze_bn,
-            *args,
             **kwargs
         )
+    
+    def __call__(self, inputs):
+        return super(ResNet2D18, self).call(inputs)
 
 
 class ResNet2D34(ResNet2D):
